f5dcde14ba57dac3b3f147bdc7e5d3ad05e59f05
DanDukes <degeare28@gmail.com>
2020-05-09 13:26:53 -0600

    added indexedDB file, service worker for caching, and a webmanifest

--- server.js
+++ server.js
@@ -3,22 +3,19 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(logger("dev"));
 
 app.use(compression());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, useUnifiedTopology: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", { useNewUrlParser: true, useUnifiedTopology: true });
 
 // routes
 app.use(require("./routes/api.js"));
